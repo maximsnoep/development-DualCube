be236ea6dea9289436ce030a2432b6e25e63fcd5
DukeBas <35384117+DukeBas@users.noreply.github.com>
2026-04-04 15:02:12 +0200

    Rough outline loops idea

--- dualcube/core/src/skeleton/generate_loops.rs
+++ dualcube/core/src/skeleton/generate_loops.rs
@@ -11,7 +11,15 @@ use crate::{
 pub fn generate_loops(skeleton: &SkeletonData, mesh: &Mesh<INPUT>) -> SlotMap<LoopID, Loop> {
     let slots: SlotMap<LoopID, Loop> = SlotMap::with_key();
 
+    // Use ortho-skeleton, for each patch boundary assign 4 points that will host loop (paths).
     // TODO
 
+    // For each patch, for each side that does not correspond to a boundary, 
+    // find a point on the surface that represents the center of that face.
+    // TODO
+
+    // Trace paths between boundaries and points to create the loops
+    // TODO
+    
     slots
 }
